3afc0f8475fb9e6b0c677c0e7d261b23be3a3e27
paper <paper.ws@medialand.tw>
2020-08-28 11:47:00 +0800

    test

--- admin-vue/js/all.js
+++ admin-vue/js/all.js
@@ -1,11 +1,26 @@
 // const chart = require('./chart.js');
+import Vue  from 'vue/dist/vue.common'
 import chart from './chart'
 import Menu from './menu'
 import filter from './filter'
 import file from './file'
 import './polyfill'
 
+let vm = new Vue({
+    el : '#app',
+    data: {
+
+    },
+    methods: {
+        
+    },
+    created(){
+        
+    }
+})
+
 new Menu();
 new filter();
 new chart();
 new file();
+
